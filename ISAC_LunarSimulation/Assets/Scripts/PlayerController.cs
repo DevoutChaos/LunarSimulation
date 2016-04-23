91e656812ffd4873a6ddc173c54fa775c5251715
Liam Faulds <chaosend50@gmail.com>
2016-04-23 15:49:06 +0100

    Player Controller
    PlayerController.cs added
    -Functionality untested

---
+++ ISAC_LunarSimulation/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    //Declarations
+    public float moveSpeed;
+    bool canMove;
+    Vector3 pos;
+    Transform tr;
+
+	// Use this for initialization
+	void Start () {
+        pos = transform.position;
+        tr = transform;
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && transform.position == pos)
+        {
+            pos += Vector3.up;
+        }
+
+        if (Input.GetKey(KeyCode.A) && transform.position == pos)
+        {
+            pos += Vector3.left;
+        }
+
+        if (Input.GetKey(KeyCode.S) && transform.position == pos)
+        {
+            pos += Vector3.down;
+        }
+
+        if (Input.GetKey(KeyCode.D) && transform.position == pos)
+        {
+            pos += Vector3.right;
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, pos, Time.deltaTime * moveSpeed);
+    }
+}
