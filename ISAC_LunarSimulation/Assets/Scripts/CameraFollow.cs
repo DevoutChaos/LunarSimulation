2a0fd35e11a1f3e047a6d294baf88168c21f41ea
Liam Faulds <chaosend50@gmail.com>
2016-04-23 17:47:52 +0100

    Stuff
    Bunch of art
    Updated player
    Camera follows appropriately

---
+++ ISAC_LunarSimulation/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    //Declarations
+    [Header("Set the player object here")]
+    public Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 cameraPosition = new Vector2(playerTransform.position.x, playerTransform.position.y);
+        transform.position = new Vector3(cameraPosition.x, cameraPosition.y, transform.position.z);
+	}
+}
--- ISAC_LunarSimulation/Assets/Scripts/PlayerController.cs
+++ ISAC_LunarSimulation/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,14 @@ public class PlayerController : MonoBehaviour {
     //Declarations
     public float moveSpeed;
     bool canMove;
+    public bool blocked;
+    public float counter;
     Vector3 pos;
     Transform tr;
 
 	// Use this for initialization
 	void Start () {
+        blocked = false;
         pos = transform.position;
         tr = transform;
 	}
@@ -20,24 +23,61 @@ public class PlayerController : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.W) && transform.position == pos)
         {
+            counter = 0;
+            blocked = false;
             pos += Vector3.up;
+            pos.x = Mathf.Round(pos.x);
+            pos.y = Mathf.Round(pos.y);
+            pos.z = 0;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.A) && transform.position == pos)
         {
+            counter = 0;
+            blocked = false;
             pos += Vector3.left;
+            pos.x = Mathf.Round(pos.x);
+            pos.y = Mathf.Round(pos.y);
+            pos.z = 0;
+            transform.rotation = Quaternion.Euler(0, 0, 90);
         }
 
         if (Input.GetKey(KeyCode.S) && transform.position == pos)
         {
+            counter = 0;
+            blocked = false;
             pos += Vector3.down;
+            pos.x = Mathf.Round(pos.x);
+            pos.y = Mathf.Round(pos.y);
+            pos.z = 0;
+            transform.rotation = Quaternion.Euler(0, 0, 180);
         }
 
         if (Input.GetKey(KeyCode.D) && transform.position == pos)
         {
+            counter = 0;
+            blocked = false;
             pos += Vector3.right;
+            pos.x = Mathf.Round(pos.x);
+            pos.y = Mathf.Round(pos.y);
+            pos.z = 0;
+            transform.rotation = Quaternion.Euler(0, 0, 270);
         }
-
-        transform.position = Vector2.MoveTowards(transform.position, pos, Time.deltaTime * moveSpeed);
+        if (!blocked && transform.position != pos)
+        {
+            counter += Time.deltaTime * moveSpeed;
+            transform.position = Vector2.MoveTowards(transform.position, pos, Time.deltaTime * moveSpeed);
+            
+            if (counter >= 1.1)
+            {
+                blocked = true;
+            }
+        }
+        if (blocked)
+        {
+            pos = transform.position;
+        }
+        
     }
 }
